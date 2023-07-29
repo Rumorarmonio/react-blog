5b00e086e81e8aa91e8209219cf410cde3b091c5
Egor Skleynov <yxllwllxy@gmail.com>
2023-07-29 09:41:27 +0800

    add dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,9 +3,27 @@ import styles from './Dialogs.module.scss'
 
 function Dialogs() {
     return (
-        <>
-            Dialogs
-        </>
+        <div className={styles.dialogs}>
+            <div className={styles.dialogItems}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Dimych
+                </div>
+                <div className={styles.dialog}>
+                    Semen
+                </div>
+                <div className={styles.dialog}>
+                    Andrey
+                </div>
+                <div className={styles.dialog}>
+                    Sasha
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>Yo!</div>
+            </div>
+        </div>
     )
 }
 
