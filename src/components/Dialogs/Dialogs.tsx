c9bc04a73c9cad3263c7460aa2d4cb6fd9fcd5a0
Egor Skleynov <yxllwllxy@gmail.com>
2023-07-29 10:02:38 +0800

    add routing for dialog page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import styles from './Dialogs.module.scss'
+import {NavLink} from 'react-router-dom'
 
 function Dialogs() {
     return (
         <div className={styles.dialogs}>
-            <div className={styles.dialogItems}>
-                <div className={styles.dialog + ' ' + styles.active}>
-                    Dimych
-                </div>
-                <div className={styles.dialog}>
-                    Semen
-                </div>
-                <div className={styles.dialog}>
-                    Andrey
-                </div>
-                <div className={styles.dialog}>
-                    Sasha
-                </div>
-            </div>
-            <div className={styles.messages}>
-                <div className={styles.message}>Hi</div>
-                <div className={styles.message}>How are you?</div>
-                <div className={styles.message}>Yo!</div>
-            </div>
+            <ul className={styles.dialogLinks}>
+                <li>
+                    <NavLink className={navData => navData.isActive ? styles.active : styles.link} to="/dialogs/1">Dimych</NavLink>
+                </li>
+                <li>
+                    <NavLink className={navData => navData.isActive ? styles.active : styles.link} to="/dialogs/2">Semen</NavLink>
+                </li>
+                <li>
+                    <NavLink className={navData => navData.isActive ? styles.active : styles.link} to="/dialogs/3">Andrey</NavLink>
+                </li>
+                <li>
+                    <NavLink className={navData => navData.isActive ? styles.active : styles.link} to="/dialogs/4">Sasha</NavLink>
+                </li>
+            </ul>
+            <ul className={styles.messages}>
+                <li className={styles.message}>Hi</li>
+                <li className={styles.message}>How are you?</li>
+                <li className={styles.message}>Yo!</li>
+            </ul>
         </div>
     )
 }
